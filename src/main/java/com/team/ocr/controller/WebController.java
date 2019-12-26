27432951bf5217250c51f8800786970457b4424e
moussaidOualid <moussaid.oualid@gmail.com>
2019-12-26 10:14:24 +0100

    qsd

--- src/main/java/com/team/ocr/controller/WebController.java
+++ src/main/java/com/team/ocr/controller/WebController.java
@@ -1,24 +1,31 @@
 package com.team.ocr.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
+
+
     @RequestMapping("/")
     @ResponseBody
     public String index() {
-        return "You made it!";
+        return "Working!";
     }
     // Login form
-  /*  @RequestMapping("/loginn")
-    public String login() {
-        return "login.html";
+    @GetMapping("/login")
+    public String resultsPage(Model model) {
+        model.addAttribute("appName", "sdg");
+        return "results";
     }
     // Login form with error
+    /*
     @RequestMapping("/login-error.html")
     public String loginError(Model model) {
         model.addAttribute("loginError", true);
